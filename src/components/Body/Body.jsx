7028d03426d5e30f3505ca56faac769c27da65b5
yusluv1234 <yusluv124@gamil.com>
2020-12-02 00:14:21 +0100

    ading body bank login image

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Mockups from '../../assets/image-mockups.png';
 
+import './body.scss';
+
 const Body = () => {
     return (
         <div className='body'>
             <div className='next-generation'>
+                <div className='next-header'>
                 Next generation digital banking
+                </div>
+                <div className='next-comment'>
+                    Take your financial life online. Your Easybank account
+                    <br />will be a one-stop-shop for spending, saving,
+                    <br />budgeting, investing, and much more
+                </div>
+                <button className='next-button'></button>
             </div>
             <div className='image-body'>
                 <img src={Mockups} alt='mochups' />
