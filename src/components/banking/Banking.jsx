cffe50fd7ce3bd0872d2c4531483502142825efa
yusluv1234 <yusluv124@gamil.com>
2020-12-03 22:04:41 +0100

    done with homepage desktop view

--- src/components/banking/Banking.jsx
+++ src/components/banking/Banking.jsx
@@ -8,28 +8,28 @@ import './Banking.scss';
 
 let Latestdata = [
     {
-        imageUrl: <img src={Currency} alt='online' width="400" height="200" />,
+        imageUrl: <img src={Currency} alt='online' width="360" height="200" />,
         name: 'By Claire Robinson',
         title: "Receive money in any currency without no fees",
         text: "The world is getting smaller and we're becoming more mobile. So while should you be forced to only receive money in a single ...",
         id: 1
     },
     {
-        imageUrl: <img src={Restaurant} alt='online' width="400" height="200" />,
+        imageUrl: <img src={Restaurant} alt='online' width="360" height="200" />,
         name: 'By Wilson Hutton',
         title: 'Treat yourself without worring about money',
         text: 'Our simple budgeting features allow you to seperate out your spending and set realistic limits each month. That means you ...',
         id: 2
     },
     {
-        imageUrl: <img src={Plane} alt='online' width="400" height="200" />,
+        imageUrl: <img src={Plane} alt='online' width="360" height="200" />,
         name: 'By Wilson Hutton',
         title: 'Take your Easybank card everywhere you go',
         text: "We want you enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you ...",
         id: 3
     },
     {
-        imageUrl: <img src={Confetti} alt='online' width="400" height="200" />,
+        imageUrl: <img src={Confetti} alt='online' width="360" height="200" />,
         name: 'By Claire Robinson',
         title: 'Our invite-only Beta account are nowlive!',
         text: "After a lot of hard work by the whole team, we're excited to launch our closed beta. Its easy to request an invite throgh the site ...",
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,48 +7,50 @@ import TwitterLogo from '../Icon/icon-twitter.svg';
 import YoutubeLogo from '../Icon/icon-youtube.svg';
 
 import './Footer.scss';
+import CustomButton from '../custom-button/CustomButton';
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='Icons-header'>
                 <div className=' icons-topbar'>
-                <a href='./homepage'>
+                <a href='/homepage'>
                     <img src={FooterLogo} alt='icons' />
                 </a>
                 </div>
                 <div className='icons-bottom'>
-                    <a href='./www.facebook.com'>
+                    <a href='/www.facebook.com'>
                     <img src={FacebookIcon} alt='icons' />
-                </a>
-                <a href='./www.twitter.com'>
+                </a> <br />
+                <a href='/www.twitter.com'>
                 <img src={TwitterLogo} alt='icons' />
-                </a>
-                <a href='./www.instagram.com'>
+                </a><br />
+                <a href='/www.instagram.com'>
                 <img src={InstagramLogo} alt='icons' />
-                </a>
-                <a href='./www.youtube.com'>
+                </a><br />
+                <a href='/www.youtube.com'>
                 <img src={YoutubeLogo} alt='icons' />
-                </a>
-                <a href='./www.pinterest.com'>
+                </a><br />
+                <a href='/www.pinterest.com'>
                 <img src={PinterestLogo} alt='icons' />
                 </a>
                 </div>
             </div>
-            <div className='footer-about1'>
-                <div className='footer-about-tag'>
-                    <a href='./about'>About</a>
-                    <a href='./contact'>Contact</a>
-                    <a href='./blog'>Blogs</a>
+            <div className='footer-about'>
+                <div className='footer-about-tag1'>
+                    <a href='/about'>About</a>
+                    <a href='/contact'>Contact</a>
+                    <a href='/blog'>Blogs</a>
                 </div>
-                <div className='footer-about-tag2'>
-                    <a href='./career'>Careers</a>
-                    <a href='./support'>Support</a>
-                    <a href='./privacy'>Privacy Policy</a>
+                <div className='footer-about-tag1'>
+                    <a href='/career'>Careers</a>
+                    <a href='/support'>Support</a>
+                    <a href='/privacy'>Privacy Policy</a>
                 </div>
             </div>
-            <div className='footer'>
-
+            <div className='footer-copy '>
+                <CustomButton title='Request Invite' />
+                <div className='copy-right' >&copy; Easybank All Rights Reserved</div>
             </div>
         </div>
     )
