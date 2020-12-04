7c5daf0d8f01917a3abce483b3b170abc3634640
yusluv1234 <yusluv124@gamil.com>
2020-12-04 19:43:41 +0100

    done with login ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
 
 import HomePage from './components/pages/homePage/HomePage';
-import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import SignIn from './components/pages/sign-in/sign-in.component';
+import SignUp from './components/pages/sign-up/sign-up.component';
+// import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
 // import Navbar from './components/navbar/Navbar';
@@ -43,12 +45,13 @@ class App extends React.Component {
         <Router>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/signin' 
-          render={() => this.props.currentUser ? ( 
-          <Redirect to='/' /> 
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
+          {/* // render={() => this.props.currentUser ? ( 
+          <Redirect to='/'
           ) : ( <SignInAndSignUpPage />
           )}
-           />
+           /> */}
         </Switch>
         </Router>
       </div>
